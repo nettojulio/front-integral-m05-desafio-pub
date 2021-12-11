78b795159302549cd5a3f457b0017bfa7d803a8f
Jessica Menezes <jessicamenezes833@gmail.com>
2021-12-11 18:13:02 -0300

    Exclusao da tabela duplicada da pagina de cobranças

--- src/components/ComponentsClients/TableCobrancasExib/index.js
+++ src/components/ComponentsClients/TableCobrancasExib/index.js
@@ -17,7 +17,7 @@ import DeleteChargeModal from "../../ComponentsDashboard/DeleteChargeModal";
 
 function TableCobrancasExib({ cardNotFound }) {
   const { loadAllBillings, loadAllClients, chargeData, handleDeleteCharge } = useFunctions();
-  const { openFilteredCard, openDeleteModal, setOpenDeleteModal, orderCharge, setOrderCharge, filter, setFilter, searchCharge } = useGlobal();
+  const { openFilteredCard, openDeleteModal, orderCharge, setOrderCharge, filter, setFilter, searchCharge } = useGlobal();
 
   useEffect(() => {
     openFilteredCard && loadAllBillings();
@@ -168,7 +168,7 @@ function TableCobrancasExib({ cardNotFound }) {
                               className="icone-excluir"
                               alt="Ícone de excluir"
                               style={{ width: 16 }}
-                              onClick={() => setOpenDeleteModal(true)}
+                              onClick={() => handleDeleteCharge(charge)}
                             />
                             Excluir
                           </div>
@@ -182,115 +182,6 @@ function TableCobrancasExib({ cardNotFound }) {
           </div>
         )
       }
-      {openDeleteModal ? (
-        <DeleteChargeModal />
-      ) : (
-        <div className="card-cobrancas">
-          <TableContainer
-            component={Paper}
-            sx={{
-              borderRadius: "30px",
-              width: "111.6rem",
-              maxHeight: "60rem",
-              overflowX: "hidden",
-              overflowY: "auto",
-            }}
-          >
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell
-                    className="cursor-pointer"
-                    onClick={() => handleChangeFilter("clients")}
-                  >
-                    <img src={inverter} alt="" />
-                    Cliente
-                  </TableCell>
-                  <TableCell
-                    className="cursor-pointer"
-                    onClick={() => handleChangeFilter("idCob")}
-                  >
-                    <img src={inverter} alt="" />
-                    ID Cob.
-                  </TableCell>
-                  <TableCell>Valor</TableCell>
-                  <TableCell>Data de venc.</TableCell>
-                  <TableCell>Status</TableCell>
-                  <TableCell>Descrição</TableCell>
-                  <TableCell> </TableCell>
-                  <TableCell> </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {searchCharge.map((charge) => (
-                  <TableRow
-                    key={charge.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {charge.cliente.nome}
-                    </TableCell>
-                    <TableCell align="left">
-                      {`${charge.id}`.padStart(9, 0)}
-                    </TableCell>
-                    <TableCell align="left">
-                      {(charge.valor / 100).toLocaleString("pt-BR", {
-                        style: "currency",
-                        currency: "BRL",
-                      })}
-                    </TableCell>
-                    <TableCell align="left">
-                      {new Date(charge.data_vencimento).toLocaleDateString(
-                        "pt",
-                        {
-                          timeZone: "UTC",
-                        }
-                      )}
-                    </TableCell>
-                    <TableCell align="center">
-                      <div
-                        className={
-                          charge.situacao === "Vencida"
-                            ? "status-red"
-                            : charge.situacao === "Pendente"
-                              ? "status-yellow"
-                              : "status-blue"
-                        }
-                      >
-                        {charge.situacao}
-                      </div>
-                    </TableCell>
-                    <TableCell align="left">{charge.descricao}</TableCell>
-                    <TableCell>
-                      <div className="container-icon-editar">
-                        <img
-                          src={editarIcon}
-                          className="icone-editar"
-                          alt="Ícone de editar"
-                          style={{ width: 16 }}
-                        />
-                        Editar
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <div className="container-icon-excluir">
-                        <img
-                          src={excluirIcon}
-                          className="icone-excluir"
-                          alt="Ícone de excluir"
-                          style={{ width: 16 }}
-                          onClick={() => handleDeleteCharge(charge)}
-                        />
-                        Excluir
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-      )}
     </>
   );
 }
